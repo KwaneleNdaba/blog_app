483e7b54dc57b85d75fc481b6fa42fc589c21143
KwaneleNdaba <113377563+KwaneleNdaba@users.noreply.github.com>
2023-09-30 19:22:18 +0200

    sidebar creation

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className='header'>
 <div className="div headerTitles">
-<span className="headerTitlesSm">React & Node</span>
+<span className="headerTitlesSm">Kwanele Prince Ndaba</span>
       <span className="headerTitlesLg">Blog</span>
 </div>
 <img className='headerImage' src = "https://images.unsplash.com/photo-1487035170094-b76fc43abdf5?ixlib=rb-4.0.3&auto=format&fit=crop&w=1600&h=500&q=60" alt = "image" />
